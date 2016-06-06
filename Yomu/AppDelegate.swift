8bdd5b5cb94d24c213a63ba74da7f40775732607
Sendy Halim <sendyhalim93@gmail.com>
2016-06-06 22:35:05 +0700

    Initial commit

---
+++ Yomu/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Yomu
+//
+//  Created by Sendy Halim on 6/6/16.
+//  Copyright © 2016 Sendy Halim. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
