5147fa1c4d54aebdcbb6b377893ba2485f46c4ce
Sendy Halim <sendyhalim93@gmail.com>
2016-06-12 21:26:25 +0700

    Add test cases for all Manga properties

--- YomuTests/Models/MangaSpec.swift
+++ YomuTests/Models/MangaSpec.swift
@@ -18,8 +18,35 @@ class MangaSpec: QuickSpec {
       let json = JSONDataFromFile("manga")!
       let manga: Manga = decode(json)!
 
+      it("should set manga slug") {
+        expect(manga.slug) == "naruto"
+      }
+
       it("Should set manga title") {
         expect(manga.title) == "Naruto"
+      }
+
+      it("should set manga author") {
+        expect(manga.author) == "KISHIMOTO Masashi"
+      }
+
+      it("should set manga image url") {
+        let endpoint = "d1/d1cd664cefc4d19ec99603983d4e0b934e8bce91c3fccda3914ac029.png"
+
+        expect(manga.image.endpoint) == endpoint
+        expect(manga.image.description) == "\(ImageURL.prefix)/\(endpoint)"
+      }
+
+      it("should set manga released year") {
+        expect(manga.releasedYear) == 1999
+      }
+
+      it("should set manga categories") {
+        expect(manga.categories) == [
+          "Azione",
+          "Shounen",
+          "Avventura"
+        ]
       }
     }
   }
