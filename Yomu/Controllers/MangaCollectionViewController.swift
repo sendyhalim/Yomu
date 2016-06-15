c0e9ee4d387fdee48a933a7b9ed6969d3c261ecf
Sendy Halim <sendyhalim93@gmail.com>
2016-06-15 21:19:51 +0700

    Add basic MangaCollectionViewController

---
+++ Yomu/Controllers/MangaCollectionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MangaCollectionViewController.swift
+//  Yomu
+//
+//  Created by Sendy Halim on 6/15/16.
+//  Copyright © 2016 Sendy Halim. All rights reserved.
+//
+
+import Cocoa
+
+class MangaCollectionViewController: NSViewController {
+  @IBOutlet weak var collectionView: NSCollectionView!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do view setup here.
+  }
+}
