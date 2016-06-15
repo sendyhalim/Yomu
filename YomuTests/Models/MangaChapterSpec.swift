94686f1d44ed98dfcf15b81e0ecd9c039b5c3f2e
Sendy Halim <sendyhalim93@gmail.com>
2016-06-16 00:08:47 +0700

    Add test case where chapter title is null

--- YomuTests/Models/MangaChapterSpec.swift
+++ YomuTests/Models/MangaChapterSpec.swift
@@ -15,20 +15,53 @@ import Quick
 class MangaChapterSpec: QuickSpec {
   override func spec() {
     describe("[Argo Decodable] When decoded from json") {
-      let jsonString = "[700, 1415346745.0, \"Uzumaki Naruto!!\", \"545c7a3945b9ef92f1e256f7\"]"
-      let json: AnyObject = JSONDataFromString(jsonString)!
-      let chapter: MangaChapter? = decode(json)
+      context("and all values are not null") {
+        let jsonString = "[700, 1415346745.0, \"Uzumaki Naruto!!\", \"545c7a3945b9ef92f1e256f7\"]"
+        let json: AnyObject = JSONDataFromString(jsonString)!
+        let chapter: MangaChapter = decode(json)!
 
-      it("should decode chapter id") {
-        expect(chapter?.id) == "545c7a3945b9ef92f1e256f7"
+        it("should decode chapter id") {
+          expect(chapter.id) == "545c7a3945b9ef92f1e256f7"
+        }
+
+        it("should decode chapter number") {
+          expect(chapter.number) == 700
+        }
+
+        it("should decode chapter title") {
+          expect(chapter.title) == "Uzumaki Naruto!!"
+        }
       }
 
-      it("should decode chapter number") {
-        expect(chapter?.number) == 700
-      }
+      context("and title is null") {
+        let json = JSON.Array([
+          JSON.Number(800),
+          JSON.Number(1888221.3),
+          JSON.Null,
+          JSON.String("someId")
+        ])
 
-      it("should decode chapter title") {
-        expect(chapter?.title) == "Uzumaki Naruto!!"
+        var chapter: MangaChapter?
+
+        switch MangaChapter.decode(json) {
+        case .Success(let _chapter):
+          chapter = _chapter
+
+        case .Failure(let error):
+          print(error)
+        }
+
+        it("should decode chapter id") {
+          expect(chapter?.id) == "someId"
+        }
+
+        it("should decode chapter number") {
+          expect(chapter?.number) == 800
+        }
+
+        it("should decode chapter title to an empty string") {
+          expect(chapter?.title) == ""
+        }
       }
     }
   }
