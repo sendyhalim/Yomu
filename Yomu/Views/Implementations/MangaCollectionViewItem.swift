4acd0c32300eb28fbdec2b10c79e50579a29c17d
Sendy Halim <sendyhalim93@gmail.com>
2016-06-15 21:51:21 +0700

    Add MangaCollectionViewItem view

---
+++ Yomu/Views/Implementations/MangaCollectionViewItem.swift
@@ -0,0 +1,14 @@
+//
+//  MangaCell.swift
+//  Yomu
+//
+//  Created by Sendy Halim on 6/15/16.
+//  Copyright © 2016 Sendy Halim. All rights reserved.
+//
+
+import Cocoa
+
+class MangaCollectionViewItem: NSCollectionViewItem {
+  @IBOutlet weak var mangaImageView: NSImageView!
+  @IBOutlet weak var titleTextField: NSTextField!
+}
