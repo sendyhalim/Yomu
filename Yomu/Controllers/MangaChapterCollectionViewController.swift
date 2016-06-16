6c1b6ce2a359acd9a5f4f51eb0727737a80f2318
Sendy Halim <sendyhalim93@gmail.com>
2016-06-16 22:39:37 +0700

    Add MangaChapterCollectionView

---
+++ Yomu/Controllers/MangaChapterCollectionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MangaChapterCollectionViewController.swift
+//  Yomu
+//
+//  Created by Sendy Halim on 6/16/16.
+//  Copyright © 2016 Sendy Halim. All rights reserved.
+//
+
+import Cocoa
+
+class MangaChapterCollectionViewController: NSViewController {
+  @IBOutlet weak var collectionView: NSCollectionView!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do view setup here.
+  }
+}
