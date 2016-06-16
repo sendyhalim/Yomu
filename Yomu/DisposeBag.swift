98d9227d084e74fade028101db42147bc70acb7a
Sendy Halim <sendyhalim93@gmail.com>
2016-06-16 23:09:41 +0700

    Add utility operator for adding Disposable to DisposeBag

---
+++ Yomu/DisposeBag.swift
@@ -0,0 +1,15 @@
+//
+//  DisposeBag.swift
+//  Yomu
+//
+//  Created by Sendy Halim on 6/16/16.
+//  Copyright © 2016 Sendy Halim. All rights reserved.
+//
+
+import RxSwift
+
+infix operator >>> { precedence 200 }
+
+func >>>(disposable: Disposable, disposeBag: DisposeBag) {
+  disposable.addDisposableTo(disposeBag)
+}
