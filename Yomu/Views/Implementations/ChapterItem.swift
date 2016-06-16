bc320ff35351c9f91391f499e42f7612de492ec3
Sendy Halim <sendyhalim93@gmail.com>
2016-06-16 23:22:21 +0700

    Add ChapterItem view

---
+++ Yomu/Views/Implementations/ChapterItem.swift
@@ -0,0 +1,13 @@
+//
+//  MangaChapterCollectionViewItem.swift
+//  Yomu
+//
+//  Created by Sendy Halim on 6/16/16.
+//  Copyright © 2016 Sendy Halim. All rights reserved.
+//
+
+import Cocoa
+
+class ChapterItem: NSCollectionViewItem {
+  @IBOutlet weak var chapterPreview: NSImageView!
+}
