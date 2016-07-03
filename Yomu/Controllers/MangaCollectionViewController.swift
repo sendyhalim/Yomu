84f8fb2516155a21c14aad4692dccd642fc99e92
Sendy Halim <sendyhalim93@gmail.com>
2016-07-04 00:05:35 +0700

    Set manga collection view data source

--- Yomu/Controllers/MangaCollectionViewController.swift
+++ Yomu/Controllers/MangaCollectionViewController.swift
@@ -25,14 +25,45 @@ class MangaCollectionViewController: NSViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
+    collectionView.dataSource = self
+
     vm.mangas
       .driveNext { [weak self] _ in
-        print("reloaded")
         self?.collectionView.reloadData()
       } >>> disposeBag
   }
 
   @IBAction func addManga(sender: NSButton) {
     vm.fetch(mangaIdField.stringValue)
+  }
+}
+
+extension MangaCollectionViewController: NSCollectionViewDataSource {
+  func numberOfSectionsInCollectionView(collectionView: NSCollectionView) -> Int {
+    return 1
+  }
+
+  func collectionView(
+    collectionView: NSCollectionView,
+    numberOfItemsInSection section: Int
+  ) -> Int {
+    return vm.count
+  }
+
+  func collectionView(
+    collectionView: NSCollectionView,
+    itemForRepresentedObjectAtIndexPath indexPath: NSIndexPath
+  ) -> NSCollectionViewItem {
+    let cell = collectionView.makeItemWithIdentifier(
+      "MangaItem",
+      forIndexPath: indexPath
+    ) as! MangaItem
+
+    let manga = vm[indexPath.item]
+
+    cell.mangaImageView.kf_setImageWithURL(manga.image.url)
+    cell.titleTextField.stringValue = manga.title
+
+    return cell
   }
 }
