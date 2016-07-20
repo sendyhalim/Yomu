5beef8e470a98f3e8c3f6cbe4d15c70c18242dab
Sendy Halim <sendyhalim93@gmail.com>
2016-07-20 23:02:32 +0700

    Add ChapterPageItem cell

---
+++ Yomu/Views/Implementations/ChapterPageItem.swift
@@ -0,0 +1,13 @@
+//
+//  ChapterPageItem.swift
+//  Yomu
+//
+//  Created by Sendy Halim on 7/20/16.
+//  Copyright © 2016 Sendy Halim. All rights reserved.
+//
+
+import Cocoa
+
+class ChapterPageItem: NSCollectionViewItem {
+  @IBOutlet weak var pageImageView: NSImageView!
+}
