786e8397be4909c87f0e6d890820076428de25fb
Sendy Halim <sendyhalim93@gmail.com>
2016-08-07 16:13:05 +0700

    Add background colour and border radius to action button

--- Yomu/Views/Implementations/ActionButton.swift
+++ Yomu/Views/Implementations/ActionButton.swift
@@ -10,11 +10,23 @@ import Cocoa
 
 class ActionButton: NSButton {
   override func viewDidMoveToWindow() {
+    wantsLayer = true
+
+    layer?.backgroundColor = NSColor(
+      calibratedRed: 0.95,
+      green: 0.95,
+      blue: 0.95,
+      alpha: 1.0
+    ).CGColor
+    layer?.cornerRadius = 5.0
+
     let paragraphStyle = NSMutableParagraphStyle()
     paragraphStyle.alignment = .Center
 
+    let color = NSColor(calibratedRed: 0.5, green: 0.5, blue: 0.5, alpha: 1.0)
+
     attributedTitle = NSAttributedString(string: title, attributes: [
-      NSForegroundColorAttributeName: NSColor.blueColor(),
+      NSForegroundColorAttributeName: color,
       NSParagraphStyleAttributeName: paragraphStyle
     ])
   }
