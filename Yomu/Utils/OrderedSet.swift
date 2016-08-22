11c475d5add2840586c3ff2193ebbae8163ae651
Sendy Halim <sendyhalim93@gmail.com>
2016-08-23 01:16:18 +0700

    Implement CustomStringConvertible, ArrayLiteralConvertible and SequenceType for OrderedSet

--- Yomu/Utils/OrderedSet.swift
+++ Yomu/Utils/OrderedSet.swift
@@ -8,20 +8,18 @@
 
 import Foundation
 
-struct OrderedSet<T: Hashable> {
-  var indexByElement: [T: Int]
-  var contents: Array<T>
+struct OrderedSet<T: Hashable>: ArrayLiteralConvertible {
+  private var indexByElement: [T: Int] = [:]
+  private var elements: Array<T> = []
+
   var count: Int {
-    return contents.count
+    return elements.count
   }
 
-  init() {
-    indexByElement = [:]
-    contents = []
-  }
-
-  subscript(index: Int) -> T {
-    return contents[index]
+  init(arrayLiteral elements: T...) {
+    for element in elements {
+      append(element)
+    }
   }
 
   mutating func append(element: T) {
@@ -29,8 +27,8 @@ struct OrderedSet<T: Hashable> {
       return
     }
 
-    indexByElement[element] = contents.count
-    contents.append(element)
+    indexByElement[element] = elements.count
+    elements.append(element)
   }
 
   mutating func remove(element: T) -> Bool {
@@ -39,12 +37,71 @@ struct OrderedSet<T: Hashable> {
     }
 
     indexByElement[element] = nil
-    contents.removeAtIndex(index)
+    elements.removeAtIndex(index)
 
     return true
   }
 
   func has(element: T) -> Bool {
     return indexByElement[element] != nil
+  }
+}
+
+extension OrderedSet: CustomStringConvertible {
+  var description: String {
+    return elements.reduce("OrderedSet \(count) objects: ") {
+      "\($0), $1"
+    }
+  }
+}
+
+extension OrderedSet: MutableCollectionType {
+  typealias Index = Int
+
+  var startIndex: Int {
+    return 0
+  }
+
+  var endIndex: Int {
+    return elements.count
+  }
+
+  subscript(index: Index) -> T {
+    get {
+      return elements[index]
+    }
+
+    set {
+      if has(newValue) {
+        return
+      }
+
+      let oldValue = elements[index]
+      indexByElement[oldValue] = nil
+      indexByElement[newValue] = index
+      elements[index] = newValue
+    }
+  }
+}
+
+extension OrderedSet: SequenceType {
+  typealias Generator = OrderedSetGenerator<T>
+
+  func generate() -> Generator {
+    return OrderedSetGenerator(set: self)
+  }
+}
+
+struct OrderedSetGenerator<T: Hashable>: GeneratorType {
+  typealias Element = T
+
+  var generator: IndexingGenerator<Array<T>>
+
+  init(set: OrderedSet<T>) {
+    generator = set.elements.generate()
+  }
+
+  mutating func next() -> T? {
+    return generator.next()
   }
 }
